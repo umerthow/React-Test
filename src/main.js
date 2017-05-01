70cbf30033bb9f94b9444367696c5979a97b76f0
umerthow <umerthow.kaitokid@gmail.com>
2017-05-01 18:11:46 +0700

    new Master React App

---
+++ src/main.js
@@ -0,0 +1,12 @@
+console.log('Hello Umar World!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
